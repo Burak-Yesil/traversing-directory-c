9e7654b978e067966049604346a433b33f612ee0
Burak Yesil <88214965+Burak-Yesil@users.noreply.github.com>
2022-03-24 21:53:41 +0000

    Functionality 1 now works

--- maxfile.c
+++ maxfile.c
@@ -4,46 +4,72 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
+void function1(char * directoryName, long int * maxWritableFileSize, long int * maxNonWritableFileSize, char * maxWritableName, char * maxNonWritableName){
+
+     DIR *dir;
+     dir = opendir(directoryName); //Opening directory
+
+     if (dir == NULL){ //checking if directory successfully opened
+          printf("Unable to open directory\n");
+          exit(1);
+     }
+     
+     printf("Function 1 Working:\n");
+
+     struct dirent *f; //stores file
+     struct stat buff; //stores file info 
+     
+     char path[4096]; //stores path
+
+
+     
+     while((f=readdir(dir)) != NULL){     
+          strcpy(path, directoryName); //Creating the actual path
+          strcat(path,"/");            //I was originally running into issues because
+          strcat(path, f->d_name);     //I was putting in the file name into stat
+
+
+          if(stat(path, &buff) != -1){ //Making sure stat works                
+               if(S_ISREG(buff.st_mode)){ //Checking if the file is a regular file 
+                    if (buff.st_mode & S_IWUSR){ //Writable file case:
+                         if (*maxWritableFileSize < buff.st_size){ //Checking if new file has max size
+                              *maxWritableFileSize = buff.st_size; 
+                              strcpy(maxWritableName, f->d_name); 
+                              continue; //Move on to next file in dir
+                         }
+                    }else{ //Non-Writable file case:
+                         if(*maxNonWritableFileSize < buff.st_size){ //Checking if new file has max size
+                              *maxNonWritableFileSize = buff.st_size;
+                              strcpy(maxNonWritableName, f->d_name);
+                              continue; //move onto next file in dir
+                         }
+                    }    
+               }
+          }
+     }
+
+     closedir(dir);
+}
+
+
+
+
+
 
 int main(int argc, char* argv[]){
 
-     DIR* dir;
-     dir = opendir(argv[1]); //Opening Directory from CLI
+     long int maxWritableFileSize = 0;
+     long int maxNonWritableFileSize = 0;
      
-     
-     struct dirent *f; //stores file
-     struct stat buff; //stores file info
-     
+     char maxWritableName[256];
+     char maxNonWritableName[256];
 
-     char maxFileNameW[1024]; //Largest writable 
-     char maxFileNameNW[1024]; //Used to keep track of largest file
-
-     
-     if (dir == NULL){ //Checking if directory exists
-     	printf("Unable to open directory\n");
-     	exit(1);
-     }
-     
-     
-     while((f=readdir(dir)) != NULL){
-     
-     	stat(f->d_name, &buff); //Getting info on the current file
-     	
-     	
-     	//printf("Mode: %d\n", buff.st_mode);
-
-     	if(S_ISREG(buff.st_mode) != 0){  //Checking that the file is a regular file
-     	printf("regular file");
-     	}
-     	
+     function1(argv[1], &maxWritableFileSize, &maxNonWritableFileSize, maxWritableName, maxNonWritableName);
   
-     	printf("%s: %ld\n", f->d_name, buff.st_size);
-     }
-     
+     printf("Writable File Name: %s, Size: %ld\n",maxWritableName, maxWritableFileSize);
+     printf("Non-Writable File Name: %s, Size: %ld\n", maxNonWritableName, maxNonWritableFileSize);
 
 
-     
-     closedir(dir);
-  
+
      return 0;
 }
